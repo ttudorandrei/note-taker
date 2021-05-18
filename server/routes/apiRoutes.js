59ffce651090646a2e6e042fe1aae9143b340ea4
Tudor Tocan <tudor.tocan@gmail.com>
2021-05-18 16:37:18 +0100

    add apiRoutes.js

---
+++ server/routes/apiRoutes.js
@@ -0,0 +1,15 @@
+const { Router } = require("express");
+
+const {
+  getNotes,
+  getNoteById,
+  updateNoteById,
+} = require("../controllers/notes");
+
+const router = Router();
+
+router.get("/notes", getNotes);
+router.get("/notes/:id", getNoteById);
+router.put("/notes.:id", updateNoteById);
+
+module.exports = router;
