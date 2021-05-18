31d45f1ae1cf648b7ed76d981f6d971102247544
Tudor Tocan <tudor.tocan@gmail.com>
2021-05-18 16:37:26 +0100

    update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@
 const express = require("express");
 const cors = require("cors");
 
+// dev dependencies
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+
 // sets the port
 const PORT = 8000;
 
@@ -14,6 +18,8 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: true }));
 app.use(express.static("public"));
+app.use("/", htmlRoutes);
+app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
