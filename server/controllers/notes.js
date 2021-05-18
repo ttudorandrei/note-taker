0bebadbcdf8b91d79913028f5c3c11b17648676a
Tudor Tocan <tudor.tocan@gmail.com>
2021-05-18 16:26:31 +0100

    add notes.js

---
+++ server/controllers/notes.js
@@ -0,0 +1,43 @@
+const getFromDb = require("../utils/getFromDb");
+const writeToDb = require("../utils/writeToDb");
+
+const getNotes = (req, res) => {
+  const notes = getFromDb();
+
+  res.json(notes);
+};
+
+const getNoteById = (req, res) => {
+  const { id } = req.params;
+  const notes = getFromDb();
+
+  const note = notes[id];
+
+  res.json(note);
+};
+
+const updateNoteById = (req, res) => {
+  const { id } = req.parmas;
+  const { body } = req;
+  const notes = getFromDb();
+  const note = notes[id];
+  const newNote = {
+    ...note,
+    ...body,
+  };
+
+  const newNotes = {
+    ...notes,
+    [id]: newNote,
+  };
+
+  writeToDb(JSON.stringify(newNotes));
+
+  res.json({ success: true });
+};
+
+module.exports = {
+  getNotes,
+  getNoteById,
+  updateNoteById,
+};
