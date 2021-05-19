7eb16917d0b33b0e0ee2bfd45942cccdd532b2df
Tudor Tocan <tudor.tocan@gmail.com>
2021-05-19 20:11:35 +0100

    fixed links to index.js and styles.css

--- server/server.js
+++ server/server.js
@@ -7,20 +7,18 @@ const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
 
 // sets the port
-const PORT = 8000;
+const PORT = process.env.PORT || 3000;
 
 // this initializes the express app
 const app = express();
 
 // Middleware
-
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: true }));
-app.use(express.static(__dirname + "../public"));
-app.use(express.static("./"));
-app.use("/", htmlRoutes);
+app.use(express.static("public"));
 app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 console.log(__dirname + "/public");
 
